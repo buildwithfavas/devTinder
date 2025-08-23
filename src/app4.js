af573148a90b682d459c414796f465a3e4af11d6
Favas <72879643+buildwithfavas@users.noreply.github.com>
2025-08-23 18:32:57 +0530

    Exploring Data sanitization,schema validation and validator library

---
+++ src/app4.js
@@ -0,0 +1,141 @@
+const express = require("express");
+require("dotenv").config();
+const connectDB = require("./config/database");
+const app = express();
+const User = require("./models/user");
+
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  // call from postman by post method pass a json object like below
+  //   {
+  //   "firstName": "Mohammed",
+  //   "lastName": "Favas",
+  //   "emailId": "mohammed.favas@example.com",
+  //   "password": "password123"
+  //   }
+
+  // or create a new instance of the User model
+  // const user = new User({
+  //     firstName: "Mohammed",
+  //     lastName: "Favas",
+  //     emailId: "mohammed.favas@example.com",
+  //     password: "password123"
+  // });
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User registered successfully");
+  } catch (error) {
+    res.status(400).send("Error registering user: " + error.message);
+  }
+});
+
+//Get user by email
+app.get("/user", async (req, res) => {
+  // call from postman by get method pass a json object emailId it will list all docs with that emailId
+  //     {
+  //     "emailId":"mohammed.favas@example.com"
+  // }
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Error fetching user: " + error.message);
+  }
+});
+
+//Get one user by email
+app.get("/userOne", async (req, res) => {
+  // call from postman by get method pass a json object emailId it will list only one doc with that emailId
+  //     {
+  //     "emailId":"mohammed.favas@example.com"
+  // }
+  const userEmail = req.body.emailId;
+  const user = await User.findOne({ emailId: userEmail });
+  if (!user) {
+    return res.status(404).send("User not found");
+  } else {
+    res.send(user);
+  }
+
+});
+
+//Feed API - GET - get all the users from the databse
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error fetching users: " + error.message);
+  }
+});
+
+//Get one user by email
+app.delete("/user", async (req, res) => {
+  // call from postman by delete method pass a json object, it will delete one doc with that objectId (NB: we passed it has an userId key)
+  //     {
+  //     "userId":"objectId here"
+  // }
+  const userId = req.body.userId;
+  try {
+    //const user = await User.findByIdAndDelete({_id: userId}); //or use short hand
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("Error fetching users: " + error.message);
+  }
+});
+
+// Update data of the user
+//send this json data to update from postman by patch method
+// {
+//     "userId":"68a8dad63c28555cab3276e1",
+//     "firstName":"changed to something",
+//     "lastName":"lastName changed"
+// }
+
+app.patch("/user/:userId", async (req, res) => {
+  //const userId = req.body.userId;
+  const userId = req.params?.userId;
+  const updateData = req.body;
+
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((key) => ALLOWED_UPDATES.includes(key));
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+
+    if(data?.skills.length > 10){
+      throw new Error("Cannot add more than 10 skills");
+    }
+
+    const user = await User.findByIdAndUpdate(userId, updateData,{
+      returnDocument: 'after',
+      runValidators: true,
+    }); //options also available learn from documentation
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Error updating user: " + error.message);
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+
+    app.listen(process.env.PORT || 777, () => {
+      console.log("Server successfully listening to port " + (process.env.PORT || 777));
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed: " + error.message);
+  });
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,68 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
     },
     lastName: {
-        type: String
+        type: String,
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email format');
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error('Weak password');
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
     },
     gender: {
-        type: String
+        type: String,
+        validate(value){
+            if(!['male', 'female', 'other'].includes(value)){
+                throw new Error('Invalid gender');
+            }
+        },
+    },
+    photoUrl: {
+        type: String,
+        default: "https://geographyandyou.com/images/user-profile.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error('Invalid URL format');
+            }
+        }
+    },
+    about: {
+        type: String,
+        default: "Hello, I'm a new user!",
+    },
+    skills: {
+        type: [String],
     }
+},
+{
+    timestamps: true,
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
